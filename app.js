32d95185fef61e9a8e05cdc107a9029a26e998ca
Velugondaiah <166354417+Velugondaiah@users.noreply.github.com>
2024-09-05 19:34:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let express = require('express')
 let app = express()
 
 app.get('/', (request, response) => {
-  response.send('Home Page')
+  response.send('Home Pag')
 });
 
 app.get("/about" , (request , response)=>{
